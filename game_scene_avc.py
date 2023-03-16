744b2987715f59318165961d88445ff02ca9a3e9
Ruoqi Huang <huangruoqi374@gmail.com>
2023-03-15 22:45:38 -0700

    change depth

--- game_scene_avc.py
+++ game_scene_avc.py
@@ -12,7 +12,7 @@ import pygame
 from pygame.locals import *  # noqa
 EMPTY = pygame.Surface([1, 1], pygame.SRCALPHA)
 
-DEPTH = 4
+DEPTH = 3
 
 class GameSceneAVC(Scene):
     def __init__(self, screen, *args, **kwargs):
