b88c923c6bac64a23a9484778740065c992cd128
Ruoqi Huang <huangruoqi374@gmail.com>
2023-03-16 18:45:22 -0700

    add 2 models

--- train.py
+++ train.py
@@ -6,8 +6,25 @@ from chess import Game
 from math import sqrt, tanh
 
 
-MODEL_NAME = 'test'
-model = tf.keras.Sequential([
+INITIAL_MODELS = [
+    'G_0012', 
+    'G_0013', 
+    'G_0014', 
+    'G_0015', 
+    'G_0016', 
+]
+MAX_WINNERS = 10
+NUM_SOLUTION = 20
+NUM_GENERATIONS = 1000
+NUM_PARENTS_MATING = 5
+
+round_numer = 0
+with open('round.txt', 'r') as f:
+    round_numer = int(f.read())
+with open('round.txt', 'w') as f:
+    f.write(str(round_numer + 1))
+
+dummy = tf.keras.Sequential([
     layers.Dense(600, input_shape=(448,), activation="relu", name="layer1"),
     layers.Dense(400, activation="relu", name="layer2"),
     layers.Dense(128, activation="relu", name="layer3"),
@@ -16,17 +33,33 @@ model = tf.keras.Sequential([
     layers.Dense(1, activation="sigmoid", name="layer6"),
 ])
 
-winner = tf.keras.Sequential([
-    layers.Dense(600, input_shape=(448,), activation="relu", name="layer1"),
-    layers.Dense(400, activation="relu", name="layer2"),
-    layers.Dense(128, activation="relu", name="layer3"),
-    layers.Dense(32, activation="sigmoid", name="layer4"),
-    layers.Dense(8, activation="sigmoid", name="layer5"),
-    layers.Dense(1, activation="sigmoid", name="layer6"),
-])
-winner_fitness = 0
-best_fitness = 0
-best_weights = model.get_weights()
+def add_to_winners(winners_, fitness, weights):
+    if len(winners_) < MAX_WINNERS:
+        model = tf.keras.Sequential([
+            layers.Dense(600, input_shape=(448,), activation="relu", name="layer1"),
+            layers.Dense(400, activation="relu", name="layer2"),
+            layers.Dense(128, activation="relu", name="layer3"),
+            layers.Dense(32, activation="sigmoid", name="layer4"),
+            layers.Dense(8, activation="sigmoid", name="layer5"),
+            layers.Dense(1, activation="sigmoid", name="layer6"),
+        ])
+        model.set_weights(weights)
+        winners_.append([fitness, model])
+    else:
+        winners_[0][0] = fitness
+        winners_[0][1].set_weights(weights)
+    winners_.sort(key=lambda x: x[0])
+
+def load_model(name):
+    model_path = os.path.join('model', name)
+    model = tf.keras.models.load_model(os.path.join('model', name))
+    info_path = os.path.join(model_path, "info.txt")
+    fitness = 0
+    with open(info_path, 'r') as f:
+        content = f.read().split()
+        assert len(content) == 2
+        fitness = float(content[1])
+    return [fitness, model]
 
 def save_model(model, name, fitness):
     model_path = os.path.join('model', name)
@@ -35,14 +68,16 @@ def save_model(model, name, fitness):
     with open(info_path, 'w') as f:
         f.write(f"<fitness> {fitness}")
 
-def fitness_func(solution, sol_idx):
-    global keras_ga, model, best_fitness, best_weights, winner, winner_fitness
-    # depth = 2
-    model_weights_matrix = pygad.kerasga.model_weights_as_matrix(model=model, weights_vector=solution)
-    model.set_weights(weights=model_weights_matrix)
-    # result = Game().run_game_avc(model, depth)
-    result = Game().run_game_ava(model, winner)
-    opponent_score = winner_fitness
+winners = [load_model(i) for i in INITIAL_MODELS]
+winners.sort(key=lambda x: x[0])
+dummy.set_weights(winners[len(winners)-1][1].get_weights())
+last_winner = winners[0]
+last_fitness = last_winner[0]
+last_weights = last_winner[1].get_weights()
+
+
+def calculate_rank_score(base, result):
+    opponent_score = base
     turn_score = -sqrt(result.turn)/16 + 1
     if result.winner is False:
         turn_score = -turn_score
@@ -53,26 +88,39 @@ def fitness_func(solution, sol_idx):
     piece_score = tanh(result.piece_score/25)
     if result.winner is None: 
         match_score = 0
-    fitness = opponent_score + turn_score + match_score + piece_score
-    print(result, fitness)
-    if fitness > best_fitness:
-        best_fitness = fitness
-        best_weights = model_weights_matrix
-    return fitness
+    return opponent_score + turn_score + match_score + piece_score
+
+
+def fitness_func(solution, sol_idx):
+    global keras_ga, dummy, last_fitness, last_weights, winners
+    model_weights_matrix = pygad.kerasga.model_weights_as_matrix(model=dummy, weights_vector=solution)
+    dummy.set_weights(weights=model_weights_matrix)
+    rank_score = 0
+    for i in range(len(winners)):
+        base, opponent = winners[i]
+        result = Game().run_game_ava(dummy, opponent)
+        rank_score += calculate_rank_score(base, result)
+        print(result)
+    rank_score /= len(winners)
+    print("{:.3f}".format(rank_score))
+    if rank_score > last_fitness:
+        last_fitness = rank_score
+        last_weights = model_weights_matrix
+    return rank_score
 
 def callback_generation(ga_instance):
-    global keras_ga, model, best_fitness, best_weights, winner, winner_fitness
+    global keras_ga, dummy, last_fitness, last_weights, winners
     print("Generation = {generation}".format(generation=ga_instance.generations_completed))
     print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution()[1]))
-    if winner_fitness < best_fitness:
-        winner_fitness = best_fitness
-        winner.set_weights(best_weights)
-        save_model(winner, f'G_{str(ga_instance.generations_completed).zfill(4)}', winner_fitness)
+    if winners[0][0] < last_fitness:
+        add_to_winners(winners, last_fitness, last_weights)
+        dummy.set_weights(last_weights)
+        save_model(dummy, f'G_{str(round_numer).zfill(2)}_{str(ga_instance.generations_completed).zfill(4)}', last_fitness)
 
-keras_ga = pygad.kerasga.KerasGA(model=model, num_solutions=20)
+keras_ga = pygad.kerasga.KerasGA(model=dummy, num_solutions=NUM_SOLUTION)
 initial_population = keras_ga.population_weights
-ga_instance = pygad.GA(num_generations=1000, 
-                       num_parents_mating=5, 
+ga_instance = pygad.GA(num_generations=NUM_GENERATIONS, 
+                       num_parents_mating=NUM_PARENTS_MATING, 
                        initial_population=initial_population,
                        fitness_func=fitness_func,
                        on_generation=callback_generation,
