90d2727a42990d0bfa07b2973632c29608df5943
Ruoqi Huang <huangruoqi374@gmail.com>
2023-03-17 15:00:04 -0700

    change initial output

--- train.py
+++ train.py
@@ -41,7 +41,7 @@ def get_initial_models():
                 fitness = float(content[1])
                 candidates.append((fitness, model_path))
     candidates.sort(reverse=True)
-    print(candidates[:MAX_WINNERS])
+    [print(i) for i in candidates[:MAX_WINNERS]]
     return [load_model(k[1]) for k in candidates[:MAX_WINNERS]]
 
 
