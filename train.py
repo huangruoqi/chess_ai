29286fb6b43690c01d2d1693f6e45d6addfe5dd4
Ruoqi Huang <huangruoqi374@gmail.com>
2023-03-21 15:36:45 -0700

    finish loading new models

--- train.py
+++ train.py
@@ -11,23 +11,18 @@ import keras.backend as K
 import numpy
 from utils import Chess_Model
 
-d = tf.config.experimental.list_physical_devices('GPU')
+d = tf.config.experimental.list_physical_devices("GPU")
 if d:
     tf.config.experimental.set_memory_growth(d[0], True)
 # tf.debugging.set_log_device_placement(True)
 
 
-
 INSTANCE = str(int(time.time()))
 MAX_WINNERS = 10
 NUM_SOLUTION = 10
-NUM_GENERATIONS = 1000
 NUM_PARENTS_MATING = 5
 DEBUG = True
 
-
-chess_model = Chess_Model()
-dummy = chess_model.get_clone()
 
 def get_initial_models():
     training_instances = os.listdir("model")
@@ -46,12 +41,12 @@ def get_initial_models():
     [print(i) for i in candidates[:MAX_WINNERS]]
     return [chess_model.load_model(k[1]) for k in candidates[:MAX_WINNERS]]
 
+
 def get_initial_population(models):
     population = []
     for fitness, model in models:
         population.append(model_weights_as_vector(model))
     return population
-
 
 
 def add_to_winners(fitness, weights):
@@ -72,16 +67,6 @@ def add_to_winners(fitness, weights):
         winners[0][1].set_weights(weights)
     winners.sort(key=lambda x: x[0])
 
-winners = get_initial_models()
-winners.sort(key=lambda x: x[0])
-dummy.set_weights(winners[len(winners) - 1][1].get_weights())
-last_winner = winners[0]
-last_fitness = last_winner[0]
-last_weights = last_winner[1].get_weights()
-previous_fitness = last_fitness
-minimax_depth = 1
-increase_minimax_depth = False
-
 
 def calculate_rank_score(base, result):
     opponent_score = base
@@ -98,14 +83,9 @@ def calculate_rank_score(base, result):
     return opponent_score + turn_score + match_score + piece_score
 
 
-game = Game()
-
-
 def fitness_func(solution, sol_idx):
     global dummy, last_fitness, last_weights, winners, increase_minimax_depth, minimax_depth, game
-    model_weights_matrix = model_weights_as_matrix(
-        model=dummy, weights_vector=solution
-    )
+    model_weights_matrix = model_weights_as_matrix(model=dummy, weights_vector=solution)
     dummy.set_weights(weights=model_weights_matrix)
     rank_score = 0
     wins = 0
@@ -124,11 +104,13 @@ def fitness_func(solution, sol_idx):
         result = game.run_game_avc(dummy, minimax_depth)
         if result.winner:
             print(f"Win against depth {minimax_depth} minimax")
-            chess_model.save_model(dummy, INSTANCE, f"WIN_MINIMAX_{minimax_depth}", rank_score + 1, True)
+            chess_model.save_model(
+                dummy, INSTANCE, f"WIN_MINIMAX_{minimax_depth}", rank_score + 1, True
+            )
             increase_minimax_depth = True
             rank_score += minimax_depth
     except Exception as e:
-        with open(os.path.join("logs", f"{int(time.time())}.txt"), 'w') as f:
+        with open(os.path.join("logs", f"{int(time.time())}.txt"), "w") as f:
             f.write(e)
     print("Rank: {:.3f} Wins: {}".format(rank_score, wins))
     if rank_score > last_fitness:
@@ -148,7 +130,9 @@ def callback_generation(ga_instance):
         add_to_winners(last_fitness, last_weights)
         previous_fitness = last_fitness
         dummy.set_weights(last_weights)
-        chess_model.save_model(dummy, INSTANCE, f"{str(generation).zfill(4)}", last_fitness, True)
+        chess_model.save_model(
+            dummy, INSTANCE, f"{str(generation).zfill(4)}", last_fitness, True
+        )
     last_fitness = 0
     # if increase_minimax_depth:
     #     if minimax_depth < 3:
@@ -167,12 +151,13 @@ def callback_generation(ga_instance):
                 fitness, model = c
                 add_to_winners(fitness, model.get_weights())
         except Exception as e:
-            with open(os.path.join("logs", f"{int(time.time())}.txt"), 'w') as f:
+            with open(os.path.join("logs", f"{int(time.time())}.txt"), "w") as f:
                 f.write(e)
     print([x[0] for x in winners])
     current = time.time()
     print(f"Time elasped: {current - start} seconds")
     start = current
+
 
 def get_new_models():
     training_instances = os.listdir("model")
@@ -193,17 +178,45 @@ def get_new_models():
     candidates.sort(reverse=True)
     return [chess_model.load_model(k[1]) for k in candidates[:4]]
 
+chess_model = Chess_Model()
+dummy = chess_model.get_clone()
+def run():
+    global chess_model, game, winners, dummy, last_winner, last_fitness, last_weights, previous_fitness, minimax_depth, increase_minimax_depth, start
+    game = Game()
+    winners = get_initial_models()
+    winners.sort(key=lambda x: x[0])
+    dummy.set_weights(winners[len(winners) - 1][1].get_weights())
+    last_winner = winners[0]
+    last_fitness = last_winner[0]
+    last_weights = last_winner[1].get_weights()
+    previous_fitness = last_fitness
+    minimax_depth = 1
+    increase_minimax_depth = False
 
-initial_population = get_initial_population(winners)
+    ga_instance = pygad.GA(
+        num_generations=20,
+        num_parents_mating=NUM_PARENTS_MATING,
+        initial_population=get_initial_population(winners),
+        fitness_func=fitness_func,
+        on_generation=callback_generation,
+        parallel_processing=None,
+    )
+    print(f"Instance: <{INSTANCE}> started!!!")
+    start = time.time()
+    ga_instance.run()
 
-ga_instance = pygad.GA(
-    num_generations=NUM_GENERATIONS,
-    num_parents_mating=NUM_PARENTS_MATING,
-    initial_population=initial_population,
-    fitness_func=fitness_func,
-    on_generation=callback_generation,
-    parallel_processing=None,
-)
-print(f"Instance: <{INSTANCE}> started!!!")
-start = time.time()
-ga_instance.run()
+    try:
+        for i, v in enumerate(winners[6:]):
+            fitness, model = v
+            chess_model.save_model(model, INSTANCE, f"{str(i).zfill(2)}", fitness)
+        os.system("git add .")
+        os.system(f'git commit -m "Instance: {INSTANCE}"')
+        os.system("git pull origin main")
+        os.system("git push origin main")
+    except Exception as e:
+        with open(os.path.join("logs", f"{int(time.time())}.txt"), "w") as f:
+            f.write(e)
+
+if __name__ == '__main__':
+    while 1:
+        run()
